4123a35882266b64c46ac0d9476446f4ff239e32
Shubham Gyawali <shubhamgyawali11@gmail.com>
2025-12-24 02:14:53 +0545

    URL

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,8 +1,8 @@
 import { io } from "socket.io-client";
-const URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001";
-const socket = io(URL || "http://localhost:3001", {
+const URL = process.env.NEXT_PUBLIC_API_URL;
+const socket = io(URL, {
   withCredentials: true,
-  transports: ["websocket", "polling"]
+  transports: ["websocket", "polling"],
 });
 
 export default socket;
